92870665cf5d0c7c2b70ca2b6d1dfe455d56885f
Ashir-Littra <ashir.b@littra.in>
2022-12-20 15:31:26 +0530

    added home component

---
+++ src/Components/TopBanner/TopBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './topbanner'
+
+const TopBanner = () => {
+  return (
+    <div className='topbanner-container'>
+
+    </div>
+  )
+}
+
+export default TopBanner
